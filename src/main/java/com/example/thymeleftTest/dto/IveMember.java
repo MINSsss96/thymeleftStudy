21cef5c572cfa2b0eb84f184a25467ba2da63f03
MINSsss96 <minsue0422@naver.com>
2025-08-04 16:52:20 +0900

    model

---
+++ src/main/java/com/example/thymeleftTest/dto/IveMember.java
@@ -0,0 +1,17 @@
+package com.example.thymeleftTest.dto;
+
+import lombok.*;
+
+//@Getter
+//@Setter
+//@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class IveMember {
+    private String memberName ;
+    private int age;
+    private String address;
+
+}
